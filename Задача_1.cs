195daa0d18eb4307cabdb077664bf47c7cd985b8
unknown <Eldar.abdaliev@bk.ru>
2024-02-01 00:50:02 +0400

    first commit

---
+++ Задача_1.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение M: ");
+        int m = int.Parse(Console.ReadLine());
+        Console.Write("Введите значение N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        PrintNumbersInRange(m, n);
+    }
+
+    static void PrintNumbersInRange(int m, int n)
+    {
+        if (m <= n)
+        {
+            Console.WriteLine(m);
+            PrintNumbersInRange(m + 1, n);
+        }
+    }
+}
